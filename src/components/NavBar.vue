774a9ae2fde3664f91e8c3738154b8a88a31280f
MokshaShah <moksha@simformsolutions.com>
2023-03-09 15:32:17 +0530

    init with vuetify and vue-google-translate

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app class="e-nav-bar">
+    <h1 class="center-text">Translate-Demo-Equilo</h1>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+
+  data: () => ({}),
+};
+</script>
+
+<style scoped>
+.e-nav-bar {
+  background: #008080 !important;
+  color: white;
+}
+.center-text {
+  text-align: center;
+  width: 100%;
+}
+</style>
